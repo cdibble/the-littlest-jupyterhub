ee2c3ba46d87091e4a8be019fc4786cf6d64fcbc
Connor Dibble <cddibble@gmail.com>
2021-03-28 10:13:58 -0700

    traefik linux-arm64 published checksum is wrong? maybe?

--- tljh/traefik.py
+++ tljh/traefik.py
@@ -21,7 +21,7 @@ traefik_version = "2.4.8"
 # record sha256 hashes for supported platforms here
 checksums = {
     "linux-amd64": "3c2d153d80890b6fc8875af9f8ced32c4d684e1eb5a46d9815337cb343dfd92e",
-    "linux_arm64": "0b48915a96e9d805882e2c4c6624e7d96bb11dfe2f38d5c730dab672923bf140"
+    "linux_arm64": "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 }
 
 def checksum_file(path):
