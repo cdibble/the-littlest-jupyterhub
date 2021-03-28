fc811a12f3eb6f790761b84322274501e99d15a9
Connor Dibble <cddibble@gmail.com>
2021-03-28 10:34:34 -0700

    acccidentally had the darwin checksum

--- tljh/traefik.py
+++ tljh/traefik.py
@@ -23,7 +23,7 @@ traefik_version = "2.4.8"
 # record sha256 hashes for supported platforms here
 checksums = {
     "linux-amd64": "3c2d153d80890b6fc8875af9f8ced32c4d684e1eb5a46d9815337cb343dfd92e",
-    "linux_arm64": "0b48915a96e9d805882e2c4c6624e7d96bb11dfe2f38d5c730dab672923bf140",
+    "linux_arm64": "0931fdd9c855fcafd38eba7568a1d287200fad5afd1aef7d112fb3a48d822fcc",
     "linux_armv7": "a9863430a3dfe1aa8c25fb0a37911a887834f4e1f7f69f85c66226c7573408d2"
 }
 
